f595cbef8770664133ccc0926377bc98085ee065
GKA-exe <krishna.gunturi03@gmail.com>
2024-04-26 09:35:42 +0530

    navbar updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Todos from './components/todos/Todos';
+import NavBar from './components/navBar/NavBar';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/signin" Component={SignIn} />
+          <Route path="/signup" Component={SignUp} />
+          <Route path="/" Component={Todos} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
