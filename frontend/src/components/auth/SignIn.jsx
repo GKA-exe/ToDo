554e4c5d86a544683fd355de21414a30beba8ff1
GKA-exe <krishna.gunturi03@gmail.com>
2024-04-28 14:31:36 +0530

    Sign in and sign up templates designed

--- frontend/src/components/auth/SignIn.jsx
+++ frontend/src/components/auth/SignIn.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
+import { Typography, Button, TextField } from "@mui/material";
+
+import "./SignIn.css";
+
 function SignIn() {
   return (
-    <div>SignIn</div>
-  )
+    <>
+      <form className="signin-form-style" noValidate autoComplete="off">
+        <Typography variant="h5">Sign-In</Typography>
+        <TextField
+          sx={{ marginTop: "10px" }}
+          id="enter-email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="enter-password"
+          sx={{ marginTop: "10px", marginBottom: "5px" }}
+          type="password"
+          label="Password"
+          variant="outlined"
+          fullWidth
+        />
+        <Button
+          className="signin-margin"
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Sign In
+        </Button>
+      </form>
+    </>
+  );
 }
 
 export default SignIn
--- frontend/src/components/auth/SignUp.jsx
+++ frontend/src/components/auth/SignUp.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 
+import { Typography, TextField, Button } from "@mui/material";
+import "./SignUp.css";
+
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <>
+      <form className="signup-form-style" noValidate autoComplete="off">
+        <Typography variant="h5">Sign-Up</Typography>
+        <TextField
+          id="enter-name"
+          sx={{ marginBottom: "5px" }}
+          type="Text"
+          label="Name"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          sx={{ marginTop: "5px", marginBottom: "5px" }}
+          id="enter-email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          id="enter-password"
+          sx={{ marginTop: "5px", marginBottom: "5px" }}
+          type="password"
+          label="Password"
+          variant="outlined"
+          fullWidth
+        />
+        <Button
+          className="signin-margin"
+          variant="contained"
+          color="primary"
+          type="submit"
+        >
+          Sign Up
+        </Button>
+      </form>
+    </>
+  );
 }
 
 export default SignUp
